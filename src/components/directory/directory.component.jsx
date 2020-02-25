bdb1018fb975ef1573cd8a19ebd754ad475a49a3
Lori McCurry <lori.mccurry@gmail.com>
2020-02-24 21:55:11 -0600

    React router basics

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
-import {sections} from '../../data/directory.data';
+import { sections } from '../../data/directory.data';
 import './directory.styles.scss';
 
 class Directory extends React.Component {
   constructor() {
     super();
-    console.log(sections, 'sections')
+    console.log(sections, 'sections');
     this.state = {
-      sections
+      sections,
     };
   }
 
   render() {
     return (
       <div className="directory-menu">
-        {this
-          .state
-          .sections
-          .map(({title, imageUrl, id, size}) => {
-            return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-          })}
+        {this.state.sections.map(({ id, ...otherSectionProps }) => {
+          return <MenuItem key={id} {...otherSectionProps} />;
+        })}
       </div>
-    )
+    );
   }
 }
 
